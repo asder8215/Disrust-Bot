4ef3601dfe037882feace86f7ba8eee1fa86a56b
Mahdi Ali-Raihan <asder8215@gmail.com>
2025-01-11 04:18:18 -0500

    Testing out discord messages with images

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,8 @@ impl EventHandler for Bot {
                 error!("Error sending message: {:?}", e);
             }
         }
+
+        // turbojpeg way if it worked ;-;
         // else if msg.content.starts_with("!test") {
         //     if let Some(image) = msg.attachments.get(0) {
         //         // Ensure the attachment is an image
@@ -104,32 +106,30 @@ impl EventHandler for Bot {
                         let response = reqwest::get(&image.url).await.expect("Failed to download image");
                         let img_bytes = response.bytes().await.expect("Failed to read image bytes");
     
-                        // Compress the image using mozjpeg's cjpeg tool, directly in memory
-                        // let status = Command::new("cjpeg")
-                        //     .arg("-quality")
-                        //     .arg("70") // Set the quality
-                        //     .arg("-outfile") // Output to stdout
-                        //     .arg("-")
-                        //     .stdin(Stdio::piped()) // Pipe input
-                        //     .stdout(Stdio::piped()) // Pipe output
-                        //     .spawn()
-                        //     .expect("Failed to spawn cjpeg process");
-
-                        // let mut comp = mozjpeg::Compress::new(mozjpeg::ColorSpace::JCS_RGB);
-                        // comp.set_size(img_width as usize, img_height as usize);
-                        // comp.set_quality(70.0);
-                        // let mut comp = comp.start_compress(Vec::new()).expect("Compress did not start");
-                        // // let mut output_buf = Vec::new();
-
-                        // comp.write_scanlines(&img_bytes.to_vec()[..]).expect("Did not write scanlines");
-
-                        // let data = comp.finish().expect("Did not finish compressing data");
-
                         let input_image = MyImage {
                             width: img_width as usize,
                             height: img_height as usize,
                             pixels: img_bytes.to_vec(), // Raw RGB pixel data here
                         };
+
+                        let attachment = CreateAttachment::url(&ctx.http, &image.url).await;
+
+                        // Send the compressed image
+                        let builder = CreateMessage::new()
+                        .content(format!(
+                            "Here's the image I'm compressing"
+                        ))
+                        .add_file(attachment.expect("Failed to receive user's image"));
+
+                        let _ = msg.channel_id
+                                    .send_message(&ctx.http, builder)
+                                    .await
+                                    .expect("Failed to send message");
+
+                        let _ = msg
+                            .channel_id
+                            .say(&ctx.http, format!("Compressing image..."))
+                            .await;
 
                         match input_image.to_mozjpeg(70).await {
                             Ok(compressed_data) => {
@@ -157,39 +157,6 @@ impl EventHandler for Bot {
                                     .await;
                             }
                         }
-                        // let mut child = status; // This is the process handle
-    
-                        // // Write the image bytes to the stdin of cjpeg
-                        // {
-                        //     let mut stdin = child.stdin.as_mut().expect("Failed to open stdin");
-                        //     stdin.write_all(&img_bytes).expect("Failed to write to stdin");
-                        // }
-    
-                        // // Read the compressed image from the stdout of cjpeg
-                        // let output_bytes = {
-                        //     let mut stdout = child.stdout.take().expect("Failed to open stdout");
-                        //     let mut output = Vec::new();
-                        //     stdout.read_to_end(&mut output).expect("Failed to read from stdout");
-                        //     output
-                        // };
-    
-                        // // Wait for the cjpeg process to finish
-                        // let _ = child.wait().expect("cjpeg process failed");
-    
-                        // Create the attachment for the compressed image
-                        // let attachment = CreateAttachment::bytes(&*data, &image.filename);
-    
-                        // // Send the compressed image
-                        // let builder = CreateMessage::new()
-                        //     .content(format!(
-                        //         "Reduced image size of {} from {} bytes to {} bytes",
-                        //         &image.filename,
-                        //         image.size,
-                        //         1024
-                        //     ))
-                        //     .add_file(attachment);
-    
-                        // let _ = msg.channel_id.send_message(&ctx.http, builder).await.expect("Failed to send message");
                 } else {
                     let _ = msg.channel_id.say(&ctx.http, "Image dimensions not found.").await;
                 }
@@ -197,43 +164,11 @@ impl EventHandler for Bot {
             else {
                 let _ = msg.channel_id.say(&ctx.http, "Attachment is not an image.").await;
             }
+            let _ = msg
+                            .channel_id
+                            .say(&ctx.http, format!("Done compressing image!"))
+                            .await;
         }
-        
-    //     } else if msg.content.starts_with("!test") {
-    //         let image = &msg.attachments[0];
-    //         let filename = &image.filename;
-    //         let img_size = image.size;
-    //         let img_width = image.width.expect("Image width not found");
-    //         let img_height = image.height.expect("Image height not found");
-            
-    //         let attachment = CreateAttachment::url(&ctx.http, &image.url).await;
-    //         // let builder = CreateAttachment::url(http, url)
-    //         let attachment = attachment.expect("Attachment not provided");
-            
-    //         let img_to_compress = turbojpeg::Image{
-    //             pixels: attachment.data.as_slice(),
-    //             width: img_width as usize,
-    //             pitch: (img_width as usize * turbojpeg::PixelFormat::RGB.size() * 2), 
-    //             height: img_height as usize,
-    //             format: turbojpeg::PixelFormat::RGB
-    //         };
-    //         let mut compressor = turbojpeg::Compressor::new().expect("Compressor should be made");
-    //         let _ = compressor.set_quality(70);
-    //         let _ = compressor.set_subsamp(turbojpeg::Subsamp::Sub2x2);
-
-    //         let mut output_buf = turbojpeg::OutputBuf::new_owned();
-
-    //         compressor.compress(img_to_compress.as_deref(), &mut output_buf).expect("Could not compress image");
-
-    //         let attachment = CreateAttachment::bytes(&*output_buf, filename);
-    //         // let builder = CreateMessage::new().content("Test").add_file(attachment.expect("Attachment not provided"));
-    //         let builder = CreateMessage::new()
-    //             .content(format!("Reduced image size of {} from {} bytes to {} bytes", filename, img_size, attachment.data.len()))
-    //             .add_file(attachment);
-    //         let _ = msg.channel_id.send_message(&ctx.http, builder).await.expect("message did not send");
-    //     }
-        
-    // }
     }
 
     // async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
